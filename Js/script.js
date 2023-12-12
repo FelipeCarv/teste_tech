f34e3dae3652dc47882d0a79303b784f60c7e289
felipecarvalho <felipe.carvalho_2016@outlook.com>
2023-12-12 19:47:26 -0300

    iniciando lógica de filtragem

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,4 @@
-//-------------- Toggle --------------
+//-------------- Toggle Menu --------------
 
 const toggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu-lateral')
@@ -8,3 +8,29 @@ toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
     main.classList.toggle('active');
 }) 
+
+
+//-------------- FILTRO RELATÓRIOS --------------
+
+function selectsFilter (){
+    let selectMes = document.querySelector('#mesPagamento').value;
+    let selectStatus = document.querySelector('#statusNota').value;
+    let selectDataEmissao = document.querySelector("#dataEmissao").value;
+    let selectDataCobranca = document.querySelector("#dataCobranca").value;
+
+    let dataEmissao = formataData(selectDataEmissao);
+    let dataCobranca = formataData(selectDataCobranca);
+
+    console.log(dataEmissao);
+    console.log(dataCobranca);
+    console.log(selectMes);
+    console.log(selectStatus);
+    
+}
+
+
+//-------------- FORMATA A DATA PADRÃO PT-BR E RESETA O FUSO HORÁRIO--------------
+function formataData(data){
+    let dataFormatar = new Date(data + 'T00:00:00');
+    return dataFormatar.toLocaleDateString('pt-BR', { timeZone: 'UTC' })
+}
